e5d725018e6ebce215663828ffb52926169c802a
Ethan Geller <ethan@cmn20.stanford.edu>
2014-10-19 18:50:05 -0700

    first push

---
+++ additive.m
@@ -0,0 +1,18 @@
+%problem 3
+function y = additive(f,Z,fs,dur,  name)
+%f: vector of frequencies in Hz
+%Z: vector of complex amplitudes A*exp(j*phi)
+%fs: sampling rate in Hz
+%dur: total duration of the signal in seconds
+%name: name of the output audio file
+%f and Z should be of the same length:
+%Z(1) corresponds to f(1) and so on.
+%Ethan Geller / Lab 2-2
+samples = dur*fs;
+signal = 1:samples;
+for n = 1:samples
+signal(n) = sum(Z.*sin(2*pi.*f*n/fs));
+end
+wavwrite(signal, fs, name);
+y = signal;
+end
---
+++ dtmf.m
@@ -0,0 +1,32 @@
+function y = dtmf(phone_number, tone_dur, fs, name)
+dmtf_x = [1477, 1336, 1209];
+dmtf_y = [697,770,852];
+samples_per_tone = fs * tone_dur;
+samples_per_pause = samples_per_tone * 0.2;
+total_samples = samples_per_tone * length(phone_number) + samples_per_pause * length(phone_number);
+
+%preallocate
+y = 1:total_samples;
+freq_x = 0;
+freq_y = 0;
+cursor = 1;
+for n = 1:length(phone_number)
+    if n < 0 || n > 9
+        'ERROR: invalid digit'
+        return
+    end
+    digit = phone_number(n);
+    if digit == 0
+        freq_x = 1336;
+        freq_y = 941;
+    else
+        freq_x = dmtf_x(mod(digit,3) + 1);
+        freq_y = dmtf_y(floor((digit-1) / 3)+1);
+    end
+    tone = additive([freq_x, freq_y], [1,1], fs, tone_dur, strcat('tone ', num2str(n)));
+    tone_end = cursor + length(tone) -1;
+    y(cursor:tone_end) = tone;
+    cursor = tone_end + samples_per_pause + 1;
+end
+wavwrite(y, fs, name);
+end
